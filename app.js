b3bb6d9b93ceacec365a7ebd313f599129429f35
Trevor L <trevorlong992@gmail.com>
2022-03-12 16:04:37 -0500

    Moved files out of Develop folder, it was bugging up the whole thing

--- app.js
+++ app.js
@@ -2,7 +2,28 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { dir } = require("console");
 const app = express();
-const port = 3003;
+const port = 3005;
 const dirPath = path.join(__dirname, "/public");
 
+//Setting Express parameters
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json);
+
+//Setting get and post routes
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(dirPath, "notes.html"));
+});
+
+//Setting index at the end because get * is weird
+app.get("*", function (req,res){
+    res.sendFile(path.join(dirPath, "index.html"));
+});
+
+
+//Setting up the server to listen for testing
+app.listen(port, function() {
+    console.log("Welcome to Port ${port}. How can I help you?");
+})
